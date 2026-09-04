f70d96fe9ad4eb80ef3717c373987ff093af6224
vivek <vivekchiguru8@gmail.com>
2026-09-04 14:54:09 +0530

    cloudrun

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.11-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+# Cloud Run uses 8080
+EXPOSE 8080
+
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 app:app
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(
+        message="Hello! App is running on Cloud Run",
+        status="success"
+    )
+
+@app.route('/health')
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
